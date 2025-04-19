cbee7ec286f7773dc07231decfc1fa16f3532056
AJ-Niro <andresninorodil@gmail.com>
2025-04-18 21:13:59 -0400

    refactor(product.pg.repository): remove raw sql to use query builder on search method

--- src/app/product/interfaces/raw-product.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface RawProduct {
-  id: number;
-  name: string;
-  category_id: number;
-  category_name: string;
-  location_id: number;
-  location_name: string;
-  relevance: number;
-};
--- src/app/product/product.pg.repository.ts
+++ src/app/product/product.pg.repository.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import ProductPgEntity from './entities/product.pg.entity';
-import CategoryPgEntity from '../category/entities/category.pg.entity';
-import LocationPgEntity from '../location/entities/location.pg.entity';
-import { RawProduct } from './interfaces/raw-product.interface';
 
 @Injectable()
 export class ProductPgRepository {
@@ -22,69 +19,30 @@ export class ProductPgRepository {
     limit: number,
     offset: number
   ): Promise<{ items: ProductPgEntity[], total: number }> {
-    const results: RawProduct[] = await this.repo.query(
-      `
-      SELECT
-        p.*,
-        c.name AS category_name,
-        l.name AS location_name,
-        (
-          CASE
-            WHEN LOWER(p.name) ILIKE LOWER('%' || $1 || '%') THEN 3
-            WHEN LOWER(c.name) ILIKE LOWER('%' || $1 || '%') THEN 2
-            WHEN LOWER(l.name) ILIKE LOWER('%' || $1 || '%') THEN 1
-            ELSE 0
-          END
-        ) AS relevance
-      FROM product p
-      INNER JOIN category c ON c.id = p.category_id
-      INNER JOIN location l ON l.id = p.location_id
-      WHERE
-        LOWER(p.name) ILIKE LOWER('%' || $1 || '%')
-        OR LOWER(c.name) ILIKE LOWER('%' || $1 || '%')
-        OR LOWER(l.name) ILIKE LOWER('%' || $1 || '%')
-      ORDER BY relevance DESC
-      LIMIT $2 OFFSET $3
-      `,
-      [query, limit, offset],
-    );
+    const qb = this.repo
+      .createQueryBuilder('p')
+      .innerJoinAndSelect('p.category', 'c')
+      .innerJoinAndSelect('p.location', 'l')
+      .addSelect(`
+      CASE
+        WHEN LOWER(p.name) ILIKE LOWER(:qLike) THEN 3
+        WHEN LOWER(c.name) ILIKE LOWER(:qLike) THEN 2
+        WHEN LOWER(l.name) ILIKE LOWER(:qLike) THEN 1
+        ELSE 0
+      END`, 'relevance')
+      .where('LOWER(p.name) ILIKE LOWER(:qLike)')
+      .orWhere('LOWER(c.name) ILIKE LOWER(:qLike)')
+      .orWhere('LOWER(l.name) ILIKE LOWER(:qLike)')
+      .orderBy('relevance', 'DESC')
+      .skip(offset)
+      .take(limit)
+      .setParameter('qLike', `%${query}%`);
 
-
-    const productsWithRelevance = results.map((rawProduct) => {
-      const product = new ProductPgEntity();
-      product.id = rawProduct.id;
-      product.name = rawProduct.name;
-
-      product.category = new CategoryPgEntity();
-      product.category.id = rawProduct.category_id;
-      product.category.name = rawProduct.category_name;
-
-      product.location = new LocationPgEntity();
-      product.location.id = rawProduct.location_id;
-      product.location.name = rawProduct.location_name;
-
-      (product as any).relevance = rawProduct.relevance;
-
-      return product;
-    });
-
-    const total = await this.repo.query(
-      `
-      SELECT COUNT(*)
-      FROM product p
-      INNER JOIN category c ON c.id = p.category_id
-      INNER JOIN location l ON l.id = p.location_id
-      WHERE
-        LOWER(p.name) ILIKE LOWER('%' || $1 || '%')
-        OR LOWER(c.name) ILIKE LOWER('%' || $1 || '%')
-        OR LOWER(l.name) ILIKE LOWER('%' || $1 || '%')
-      `,
-      [query],
-    );
+    const [items, total] = await qb.getManyAndCount();
 
     return {
-      items: productsWithRelevance,
-      total: parseInt(total[0].count),
+      items: items,
+      total: total,
     };
   }
 
